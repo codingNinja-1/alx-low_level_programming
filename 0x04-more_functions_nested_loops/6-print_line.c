031e445a242bfef4fde6e16e062a0955d023d94a
Timilehin Aruaji <arutimzy15@gmail.com>
2023-09-14 16:43:41 -0700

    nested

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,3 +1,4 @@
+#include "main.h"
 
 /**
  * print_line - draws a straight line in the terminal
@@ -5,18 +6,18 @@
  */
 void print_line(int n)
 {
-if (n <= 0)
-{
-_putchar('\n');
-} else
-{
-int i;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
 
-for (i = 1; i <= n; i++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
 
 }
