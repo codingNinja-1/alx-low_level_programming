ce8c2223dfa4f9ddf3b0a25a66182087cddb2716
Timilehin Aruaji <arutimzy15@gmail.com>
2023-09-14 07:13:45 -0700

    update

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,3 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
@@ -9,4 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif
