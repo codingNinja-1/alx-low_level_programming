c6c1fb63ad50ce8b537a67cc8cad7941f1e5ee38
Timilehin Aruaji <arutimzy15@gmail.com>
2023-09-21 11:56:18 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,7 @@ char *cap_string(char *str)
 	int index = 0;
 
 	while (str[index])
-	{
+{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
@@ -32,7 +32,7 @@ char *cap_string(char *str)
 			str[index] -= 32;
 
 		index++;
-	}
+}
 
 	return (str);
 }
